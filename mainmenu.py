49881137461b215002148aeaed0508081a62671e
roger <igor.onyshchenko@gmail.com>
2012-01-18 15:57:24 +0200

    Main menu get method was fixed

--- mainmenu.py
+++ mainmenu.py
@@ -3,7 +3,7 @@ import cherrypy
 
 
 def get():
-    if len(cherrypy.request.cookie) > 0 and req.cookie[constants.FACEBOOK_CODE]:
+    if len(cherrypy.request.cookie) > 0 and cherrypy.request.cookie[constants.FACEBOOK_CODE]:
         return '&nbsp;<a href="/addtodo">add todo</a>' \
             '&nbsp;<a href="/listtodo">list todo</a>' \
             '&nbsp;<a href="/logout">logout</a>'
