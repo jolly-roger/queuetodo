a86b0cd84a8e2348ecf720e10df20b78676a2de1
roger <igor.onyshchenko@gmail.com>
2012-01-19 17:38:05 +0200

    Fix authentication

--- facebook/authentication.py
+++ facebook/authentication.py
@@ -6,13 +6,13 @@ from . import constants
 CALLBACK_URL = "http://dns-dig.net/authenticatecallback"
 
 def deauthenticate():
-    cherrypy.response.cookie[constants.FACEBOOK_ACCESS_TOKEN] = cherrypy.response.cookie[constants.FACEBOOK_ACCESS_TOKEN]
+    cherrypy.response.cookie[constants.FACEBOOK_ACCESS_TOKEN] = cherrypy.request.cookie[constants.FACEBOOK_ACCESS_TOKEN]
     cherrypy.response.cookie[constants.FACEBOOK_ACCESS_TOKEN]['expires'] = 0
 
 def authenticate():
     raise cherrypy.HTTPRedirect("https://graph.facebook.com/oauth/access_token?" \
         "client_id=" + constants.APP_ID + "&redirect_uri=" + CALLBACK_URL + \
-        "client_secret=" + constants.APP_SECRET + "&code=" + cherrypy.request.cookie[constants.FACEBOOK_CODE])
+        "client_secret=" + constants.APP_SECRET + "&code=" + cherrypy.response.cookie[constants.FACEBOOK_CODE])
     
 def callbackHandler(access_token, expires):
     cherrypy.response.cookie[constants.FACEBOOK_ACCESS_TOKEN] = access_token
