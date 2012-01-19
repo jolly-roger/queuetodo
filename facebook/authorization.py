a59a5d6f9d7ad2450840f0c69c25cbdae89f0129
roger <igor.onyshchenko@gmail.com>
2012-01-19 16:56:44 +0200

    Fix facebook constants import

--- facebook/authorization.py
+++ facebook/authorization.py
@@ -1,5 +1,6 @@
 import cherrypy
-import constants
+
+from constants import APP_ID
 
 
 FACEBOOK_CODE = 'facebook_code'
@@ -11,7 +12,7 @@ def logout():
     
 def signin():
     raise cherrypy.HTTPRedirect("https://www.facebook.com/dialog/oauth?" \
-        "client_id=" + constants.APP_ID + "&redirect_uri=" + CALLBACK_URL)
+        "client_id=" + APP_ID + "&redirect_uri=" + CALLBACK_URL)
     
 def callbackHandler(code):
     cherrypy.response.cookie[FACEBOOK_CODE] = code
