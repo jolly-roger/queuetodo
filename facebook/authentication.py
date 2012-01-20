98fd89357160b18aecb42e63f953d208819435f5
roger <igor.onyshchenko@gmail.com>
2012-01-20 18:40:12 +0200

    Fix

--- facebook/authentication.py
+++ facebook/authentication.py
@@ -19,7 +19,7 @@ def authenticate(code):
     
     raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?" \
         "client_id=" + constants.APP_ID + "&redirect_uri=" + CALLBACK_URL + \
-        "&client_secret=" + constants.APP_SECRET + "&code=" + code), encoding="utf-8")
+        "&client_secret=" + constants.APP_SECRET + "&code=" + code).read(), encoding="utf-8")
     
     access_data = urllib.parse.parse_qs(raw_access_data)
     
