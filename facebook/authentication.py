447c6bbd1a9e2f5e8b5f8b0373c7a04792b63617
roger <igor.onyshchenko@gmail.com>
2012-01-20 17:41:09 +0200

    Fix auth

--- facebook/authentication.py
+++ facebook/authentication.py
@@ -11,9 +11,8 @@ def deauthenticate():
 
 def authenticate(code):
     raise cherrypy.HTTPRedirect("https://graph.facebook.com/oauth/access_token?" \
-        "client_id=" + constants.APP_ID + \
-        "&client_secret=" + constants.APP_SECRET + "&code=" + code + \
-        "&redirect_uri=" + CALLBACK_URL)
+        "client_id=" + constants.APP_ID + "&redirect_uri=" + CALLBACK_URL + \
+        "&client_secret=" + constants.APP_SECRET + "&code=" + code)
     
 def callbackHandler(access_token, expires):
     cherrypy.response.cookie[constants.FACEBOOK_ACCESS_TOKEN] = access_token
