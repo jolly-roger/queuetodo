272cb36be7228847a0ce73ae907ecb01f2521215
roger <igor.onyshchenko@gmail.com>
2012-01-23 17:51:08 +0200

    Fix apostrofy sql problem

--- dal.py
+++ dal.py
@@ -4,7 +4,8 @@ import psycopg2
 def addtodo(userid, name):
     conn = psycopg2.connect(constants.DB_CONNECTION)
     cur = conn.cursor()
-    cur.execute("insert into todo (name, user_id) values ('" + name + "', " + userid + ");")
+    inserttodo = "insert into todo (name, user_id) values (%s, %s);" 
+    cur.execute(inserttodo, (name, userid))
     conn.commit()
     cur.close()
     conn.close()
@@ -12,27 +13,28 @@ def addtodo(userid, name):
 def getlisttodos(userid):
     conn = psycopg2.connect(constants.DB_CONNECTION)
     cur = conn.cursor()
-    cur.execute("select * from todo where user_id = " + userid + ";")
+    selecttodos = "select * from todo where user_id = %s;"
+    cur.execute(selecttodos, (userid,))
     todos = cur.fetchall()
     cur.close()
     conn.close()
     
     return todos
 
-def addcomment(todoid, name):
-    conn = psycopg2.connect(constants.DB_CONNECTION)
-    cur = conn.cursor()
-    cur.execute("insert into comments (todo_id, name) values ('" + todoid + "', " + name + ");")
-    conn.commit()
-    cur.close()
-    conn.close()
-    
-def getlistcomments(todoid):
-    conn = psycopg2.connect(constants.DB_CONNECTION)
-    cur = conn.cursor()
-    cur.execute("select * from comments where todo_id = " + todoid + ";")
-    comments = cur.fetchall()
-    cur.close()
-    conn.close()
-    
-    return comments
+#def addcomment(todoid, name):
+#    conn = psycopg2.connect(constants.DB_CONNECTION)
+#    cur = conn.cursor()
+#    cur.execute("insert into comments (todo_id, name) values ('" + todoid + "', " + name + ");")
+#    conn.commit()
+#    cur.close()
+#    conn.close()
+#    
+#def getlistcomments(todoid):
+#    conn = psycopg2.connect(constants.DB_CONNECTION)
+#    cur = conn.cursor()
+#    cur.execute("select * from comments where todo_id = " + todoid + ";")
+#    comments = cur.fetchall()
+#    cur.close()
+#    conn.close()
+#    
+#    return comments
