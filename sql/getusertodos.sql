b295e54fd63f2567d2241d1ff5ed5f0363b8b0c7
roger <igor.onyshchenko@gmail.com>
2012-02-14 16:48:42 +0200

    Getusertodos was fixed

--- sql/getusertodos.sql
+++ sql/getusertodos.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE FUNCTION getusertodos(userid bigint, statusid bigint) returns setof todo AS
 $BODY$
 BEGIN
-    select * from todo as t where t.id_todo in (select tu.todo_id from todo_user as tu where tu.user_id = userid) and
+    return query select * from todo as t
+        where t.id_todo in (select tu.todo_id from todo_user as tu where tu.user_id = userid) and
         t.status_id = statusid;
 END;
 $BODY$
