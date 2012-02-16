a2bb547d4122c8dca87e22d64dc2612c0b70147b
roger <igor.onyshchenko@gmail.com>
2012-02-16 12:07:16 +0200

    Base dal constructor was fixed

--- dal/base.py
+++ dal/base.py
@@ -5,8 +5,8 @@ from . import constants
 
 class base(object):
     def __init__(self):
-        conn = psycopg2.connect(constants.DB_CONNECTION)
-        cur = conn.cursor()
+        self.conn = psycopg2.connect(constants.DB_CONNECTION)
+        self.cur = conn.cursor()
     
     def close(self):
         self.cur.close()
