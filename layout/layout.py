95832382d0d8c7abb8c64ebabbb54855f75a27b0
roger <igor.onyshchenko@gmail.com>
2012-02-19 16:29:19 +0200

    Todo list template was extended

--- layout/layout.py
+++ layout/layout.py
@@ -28,20 +28,6 @@ def getSharedTodos(todos):
     return getTodoList(todos, None, "Shared")
 
 def getTodoList(todos, friends, title):
-    todoslayout = ""
-    friendslayout = ""
-        
-    for todo in todos:
-        todoslayout += "<div class=\"todo\" >" + \
-            "<input type=\"hidden\" class=\"todoid\" value=\"" + str(todo[0]) + "\" />" + \
-            todo[1] + "</div>"
-        
-    if not friends == None:
-        for friend in friends["data"]:
-            friendslayout += "<div class=\"friend\" >" + \
-                "<input type=\"hidden\" class=\"friendid\" value=\"" + friend["id"] + "\" />" + \
-                friend["name"] + "</div>"
-    
     tmpl = env.get_template("todoList.html")
     
-    return tmpl.render(title = title, todoslayout = todoslayout, friendslayout = friendslayout)
+    return tmpl.render(title = title, todos = todos, friends = friends)
