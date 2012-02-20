d20fb27c63a5072e9c0557888209520e20ed7308
roger <igor.onyshchenko@gmail.com>
2012-02-20 17:12:57 +0200

    Test

--- layout/layout.py
+++ layout/layout.py
@@ -14,6 +14,10 @@ def getSignin():
 def getAddTodo():
     tmpl = env.get_template("addTodo.html")
     return tmpl.render()
+    
+def getMytodos(todos, friends, statuses):
+    tmpl = env.get_template("todoList.html")    
+    return tmpl.render(title = title, todos = todos, friends = friends, statuses = statuses)
 
 def getInitTodos(todos, friends):
     return getTodoList(todos, friends, "Current")
