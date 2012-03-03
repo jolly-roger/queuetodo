167eba4e342a759f80ae76ed290e0a89c01cee60
roger <igor.onyshchenko@gmail.com>
2012-03-03 15:43:22 +0200

    FIx

--- dal/entities/todo.py
+++ dal/entities/todo.py
@@ -1,5 +1,5 @@
 class todo(object):
-    def __int__(self, id = -1, name = "", statusId = -1, statusName = "", isShared = false, priority = -1):
+    def __int__(self, id = -1, name = "", statusId = -1, statusName = "", isShared = False, priority = -1):
         self.id = id
         self.name = name
         self.statusId = statusId
