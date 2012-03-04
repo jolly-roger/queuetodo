d0d26e852a7bd1d007831c7caa9357e14b6e878d
roger <igor.onyshchenko@gmail.com>
2012-03-04 14:02:37 +0200

    Test

--- queuetodo.py
+++ queuetodo.py
@@ -80,7 +80,7 @@ class queuetodo(object):
             authorization.callbackHandler(code)
             authentication.authenticate(code)
             
-            raise cherrypy.HTTPRedirect("http://dig-dns.net")
+            raise cherrypy.HTTPRedirect("http://dns-dig.net")
             
     @cherrypy.expose
     def javascript(self):
