c4dcafebf1f4c22b2829a0ad7ff7af3cd3ba934e
roger <igor.onyshchenko@gmail.com>
2012-03-04 11:59:21 +0200

    Status entity was added

---
+++ dal/entities/status.py
@@ -0,0 +1,5 @@
+class status(object):
+    def __init__(self, id = -1, name = "", priority = -1):
+        self.id = id
+        self.name = name
+        self.priority = priority
--- dal/status.py
+++ dal/status.py
@@ -1,12 +1,23 @@
 from . import base
 from . import constants
 
+from dal.entities import status
+
+
 class status(base.base):
     def __init__(self):
         base.base.__init__(self)    
     
     def getall(self):
         self.cur.execute(constants.GET_ALL_STATUSES)
-        statuses = self.cur.fetchall()
+        rawstatuses = self.cur.fetchall()
         
+        return self._getStatusList(rawstatuses)
+    
+    def _getStatusList(self, rawstatuses):
+        statuses = []
+        
+        for rawstatus in rawstatuses:
+            statuses[len(statuses):] = [status.status(rawstatus[0], rawstatus[1], rawstatus[2])]
+            
         return statuses
