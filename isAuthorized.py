0500d87afdb3d6d608d91da9ba15c86065b54924
roger <igor.onyshchenko@gmail.com>
2012-03-14 12:04:40 +0200

    Test

--- isAuthorized.py
+++ isAuthorized.py
@@ -8,7 +8,10 @@ from layout import layout
 
 def isAuthorized(f):
     def _isAuthorized(*args, **kwargs):
-        if cherrypy.session.get(constants.IS_SIGNED_REQUEST) == None:
+        
+        cherrypy.log.error('"' + str(cherrypy.session.get(constants.IS_SIGNED_REQUEST)) + '"')
+        
+        if cherrypy.session.get(constants.IS_SIGNED_REQUEST):
             cherrypy.session[constants.IS_SIGNED_REQUEST] = False
             
             for kw in kwargs:
