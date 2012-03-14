3ba288e9ba994cba59431e72ff0419e88cd5458c
roger <igor.onyshchenko@gmail.com>
2012-03-14 16:30:17 +0200

    Test

--- queuetodo.py
+++ queuetodo.py
@@ -137,7 +137,7 @@ class queuetodo(object):
         todos = tdl.getmy(user.getUserId(), statusid)
         tdl.close()
         
-        return json.dumps(todos[0].statusName)
+        return json.dumps(todos[0].isShared)
 
 
 queuetodoconf = os.path.join(os.path.dirname(__file__), "queuetodo.conf")
