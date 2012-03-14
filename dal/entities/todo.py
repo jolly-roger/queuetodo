c6e48f3f767af15c188658304901224f6ee38eb8
roger <igor.onyshchenko@gmail.com>
2012-03-14 16:40:40 +0200

    Test

--- dal/entities/todo.py
+++ dal/entities/todo.py
@@ -1,8 +1,15 @@
 class todo(object):
     #def __init__(self, id = -1, name = "", statusId = -1, statusName = "", isShared = False, priority = -1):
-        self.id = id
-        self.name = name
-        self.statusId = statusId
-        self.statusName = statusName
-        self.isShared = isShared
-        self.priority = priority
+        #self.id = id
+        #self.name = name
+        #self.statusId = statusId
+        #self.statusName = statusName
+        #self.isShared = isShared
+        #self.priority = priority
+        
+    self.id = -1
+    self.name = ""
+    self.statusId = -1
+    self.statusName = ""
+    self.isShared = False
+    self.priority = -1
